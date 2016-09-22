51009255c1d5033dd1ab2470be9876dbb1777990
jaloar <jaloar@jasonloar.com>
2016-09-22 11:42:33 -0400

    added homepage and rps

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   
+  resources :answers
+
+  get 'answers' => 'answers#index'
+
   root 'home#homepage'
 
   get 'homepage' => 'home#homepage'
 
   get 'home' => 'home#index'
+
+  get 'rps' => 'rockpaperscissors#index'
   
   devise_for :users
 
-  mount HowTo::Engine => '/answers'
+  # mount HowTo::Engine => '/answers'
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922010942) do
+ActiveRecord::Schema.define(version: 20160922135652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "how_to_content_translations", force: :cascade do |t|
     t.integer  "how_to_content_id"
